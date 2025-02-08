64962140c8d0c559f50cfff6eae2489d2faa8d16
Phuc Vinh <nguyenvinh19525@gmail.com>
2025-02-08 23:00:29 +0700

    fix-landing-page

--- js/landing-page.js
+++ js/landing-page.js
@@ -104,4 +104,84 @@ if (navbarButton)
         }   
     })
 }
+// animation scroll and load
+// kiểm tra phần tử trong viewportviewport
+function isInViewport(element)
+{
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top + rect.height / 2 <= window.innerHeight &&
+        rect.bottom - rect.height / 2 >= 0
+    );
+}
+// xử lý scroll hoặc load
+const ScrollLoad = (element, animation, state) =>
+{
+    if (isInViewport(element) && !state.hasAnimation)
+    {
+        element.classList.add(animation);
+        state.hasAnimation = true;
+    }
+}
+const handleScrollLoad = (parent, child, animation, state) =>
+{
+    if (isInViewport(parent) && !state.hasAnimation)
+    {
+        child.classList.add(animation);
+        state.hasAnimation = true;
+    }
+}
+const featuresScrollLoad = (element, features, animation, state) =>
+{
+    if (isInViewport(element) && !state.hasAnimation)
+    {
+        features.forEach((item, index) =>
+        {
+            setTimeout(()=>
+            {
+                item.classList.add(animation);
+            }, index*400)
+        })
+        state.hasAnimation = true; // đánh dấu thực hiện animation
+    }
+}
+const priceScrollLoad = (element, priceTypes, animation, state) =>
+{
+    if (isInViewport(element) && !state.hasAnimation)
+    {
+        priceTypes.forEach((index, item) =>
+        {
+            setTimeout(() =>
+            {
+                item.classList.add(animation);
+            }, index*400)
+        })
+        state.hasAnimation = true;
+    }
+}
+const discoverState = {hasAnimation: false};
+const portfolioState = {hasAnimation: false};
+const featuresState = {hasAnimation: false};
+const pricingState = {hasAnimation: false};
+const contactState = {hasAnimation: false};
+
+// querySelector
+const discoverImg = document.querySelector('.discover-image');
+const portfolioBox = document.querySelector('.portfolio-animation');
+const featureSection = document.querySelector('.feature-section');
+const featureItems = featureSection.querySelectorAll('.col-features');
+const features = [...featureItems];
+const priceSection = document.querySelector('.price-section');
+const priceBoxes = priceSection.querySelectorAll('.price-box');
+const priceArray = [...priceBoxes];
+const contactSection = document.querySelector('.contact-section');
+const contactBox = contactSection.querySelector('.contact-box');
+window.addEventListener("scroll", () =>
+{
+    ScrollLoad(discoverImg, "slideInLeft", discoverState);
+    ScrollLoad(portfolioBox, "fadeIn", portfolioState);
+    featuresScrollLoad(featureSection, features, "fadeIn", featuresState);
+    featuresScrollLoad(priceSection, priceArray, "bounceIn", pricingState);
+    handleScrollLoad(contactSection, contactBox, "bounceIn", contactState);
+})
 
